b87ca0180b8c3117e2269086003dc6ad81f4d2b2
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-09-19 10:00:50 +0200

    made tower able to shoot projectile

---
+++ Assets/Scripts/Towers/TowerController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    [Header("Triggers")]
+    public bool shoot = false;
+
+    [Header("Information")]
+    [SerializeField] private GameObject target;
+    [SerializeField] private GameObject turret;
+
+    [Space]
+
+    [Header("Settings")]
+    public float damage = 1f;
+    public float health = 1f;
+    public float projSpeed = 1f;
+    public GameObject projectile;
+    [Range(1.0f, 15.0f)] public float TargetRadius;
+    [Range(20.0f, 75.0f)] public float LaunchAngle;
+    [Range(0.0f, 10.0f)] public float TargetHeightOffsetFromGround;
+    [Range(0.0f, 5.0f)] public float spawnHeightOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform c in transform)
+        {
+            if(c.name == "Turret")
+            {
+                turret = c.gameObject;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shoot)
+        {
+            shoot = false;
+            ShootEnemy(target);
+        }
+    }
+
+    void ShootEnemy(GameObject t)
+    {
+        Debug.Log("Fire!");
+
+        Vector3 spawnPos = new Vector3(turret.transform.position.x, turret.transform.position.y + spawnHeightOffset, turret.transform.position.z);
+        Projectile proj = Instantiate(projectile, spawnPos, transform.rotation).GetComponent<Projectile>(); //Create new projectile
+
+        proj.TargetObjectTF = t.transform; //Set target
+
+        //Projectile settings
+        proj.TargetRadius = TargetRadius;
+        proj.LaunchAngle = LaunchAngle;
+        proj.TargetHeightOffsetFromGround = TargetHeightOffsetFromGround;
+
+        proj.Launch(); //Launch projectile
+
+        
+
+    }
+}
