993f4322205b7e848080c7d35b560532a0a83461
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-11-16 10:37:56 +0100

    added debug and fixed main menu bug

--- Assets/Scripts/General/LoadLevel.cs
+++ Assets/Scripts/General/LoadLevel.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class LoadLevel : MonoBehaviour
 {
     public string nextLevel;
+    public bool debug = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,32 @@ public class LoadLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (debug)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                LoadMainMenu();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                SceneManager.LoadScene("Level01");
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                SceneManager.LoadScene("Level02");
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                ResetLevel();
+            }
+        }
         
+    }
+
+    public void ResetLevel()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(sceneName);
     }
 
     public void LoadNextLevel()
